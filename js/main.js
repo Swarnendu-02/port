ebb993836daf1ba4586443e82fce75747a4a1e0e
Swarnendu Ghosh <60955703+Swarnendu-02@users.noreply.github.com>
2026-07-16 23:13:13 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -27,20 +27,23 @@ let visual = gsap.timeline({
 });
 
 // Reveal laptop image
-visual.to(".laptop_img", {
-  clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-  duration: 1,
-  ease: "power3.out"
-});
 visual.from(".visual_wrap p", {
   opacity: 0,
   y: 50,
   duration: 1,
   ease: "power3.out"
 });
+visual.to(".laptop_img", {
+
+  clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+  duration: 3,
+  ease: "power3.out"
+});
+
 // Scale out and fade
 visual.to(".visual_wrap_inner", {
   scale: 100,
+  duration: 3,
   ease: "power2.in"
 });
 
@@ -54,6 +57,55 @@ gsap.to(".project_top", {
     scrub: true                   // Syncs smoothly with the scroll momentum
   }
 });
+
+gsap.to(".skill_wrap ul li", {
+  transform: 'translate(0px, 0px)',
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".skill_wrap",
+    toggleActions: "play pause reverse pause", // The parent section controlling the bounds
+    start: "top 80%",
+    end: "top 40%",
+    scrub: true,               // Syncs smoothly with the scroll momentum
+  }
+});
+gsap.from(".service_box", {
+  transform: 'translateX(calc(var(--padding) / -1))',
+  ease: "power2.inOut",
+  stagger: 2,
+  opacity: 0,
+  duration: 5,
+  scrollTrigger: {
+    trigger: ".service_wrap",
+    toggleActions: "play pause reverse pause", // The parent section controlling the bounds
+    start: "top 50%",
+    end: "top 10%",
+    scrub: true,               // Syncs smoothly with the scroll momentum
+  }
+});
+gsap.from(".service_left img", {
+  transform: 'translateX(-50%)',
+  ease: "power2.inOut",
+  duration: 5,
+  scrollTrigger: {
+    trigger: ".service_wrap",
+    toggleActions: "play pause reverse pause", // The parent section controlling the bounds
+    start: "top 100%",
+    end: "top 0%",
+    scrub: true,               // Syncs smoothly with the scroll momentum
+  }
+});
+// gsap.to(".service_wrap .heading", {
+//   transform: 'translate(0px, 0px)',
+//    duration: 1,
+//   ease: "power2.inOut",
+//   scrollTrigger: {
+//     trigger: ".service_wrap", // The parent section controlling the bounds
+//     start: "top 50%",
+//     end: "+=50%",
+//     scrub: true,               // Syncs smoothly with the scroll momentum
+//   }
+// });
 
 
 
